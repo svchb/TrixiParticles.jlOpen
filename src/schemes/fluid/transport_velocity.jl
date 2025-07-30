4e40e50bd8d59422ac0f66f77e7a812916393872
Sven Berger <berger.sven@gmail.com>
2025-07-31 01:25:48 +0200

    update

--- src/schemes/fluid/transport_velocity.jl
+++ src/schemes/fluid/transport_velocity.jl
@@ -44,12 +44,12 @@ end
 function write_v0!(v0, system::FluidSystem, ::TransportVelocityAdami)
     (; initial_condition) = system
 
-    for particle in eachparticle(system)
+    # for particle in eachparticle(system)
         # Write particle velocities
         for dim in 1:ndims(system)
-            v0[ndims(system) + dim, particle] = initial_condition.velocity[dim, particle]
+            v0[ndims(system) + dim, :] = initial_condition.velocity[dim, :]
         end
-    end
+    # end
 
     return v0
 end
